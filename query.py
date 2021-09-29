49ee30dbe7055670aff9c9f830c8f186b9a00984
Ubuntu <admincruiz@datashop.ni22fioccyzefl4yrag2h112eh.ax.internal.cloudapp.net>
2021-09-29 20:26:10 +0000

    updates

--- query.py
+++ query.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import numpy as np
-from rank_bm25 import BM25Okapi
-from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import CountVectorizer
+#from rank_bm25 import BM25Okapi
+#from nltk.corpus import stopwords
+#from sklearn.feature_extraction.text import CountVectorizer
 import string
 import torch as torch
 
 from sentence_transformers import SentenceTransformer, util
 model = SentenceTransformer('Sahajtomar/sts-GBERT-de')
 
-
+print("hi")
 
 file_one=pd.read_csv("data.csv")
 
@@ -22,6 +22,7 @@ query="Logiernächte"
 
 embeddings1 = model.encode(query, convert_to_tensor=True)
 embeddings2 = model.encode(file_one.text, convert_to_tensor=True)
+print("converted")
 cosine_scores = util.pytorch_cos_sim(embeddings1, embeddings2)
 print(cosine_scores)
 
@@ -35,6 +36,5 @@ print(which)
 
 
 attribution.append(str(file_one.title_slug[int(which)])+" ("+str(int(100*max_value))+")")
-    
 
 print(attribution)
