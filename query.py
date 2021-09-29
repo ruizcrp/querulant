1c37bd73a58a51da1b29625a1b874ff1c3576d78
Christian Ruiz <ruiz.crp@gmail.com>
2021-09-29 21:54:33 +0000

    streamlit working

--- query.py
+++ query.py
@@ -1,40 +1,34 @@
+import streamlit as st
 import pandas as pd
 import numpy as np
-#from rank_bm25 import BM25Okapi
-#from nltk.corpus import stopwords
-#from sklearn.feature_extraction.text import CountVectorizer
 import string
 import torch as torch
+import time
 
 from sentence_transformers import SentenceTransformer, util
 model = SentenceTransformer('Sahajtomar/sts-GBERT-de')
 
-print("hi")
-
 file_one=pd.read_csv("data.csv")
+st.session_state.embeddings2 =  model.encode(file_one.text, convert_to_tensor=True)
+
+def lookup_query(query,embeddings2):
+    start = time.time()
+    embeddings1 = model.encode(query, convert_to_tensor=True)
+    cosine_scores = util.pytorch_cos_sim(embeddings1, embeddings2)
+    #print(cosine_scores)
+
+    max_value = torch.max(cosine_scores)
+    print(max_value)
+    which=torch.argmax(cosine_scores)
+    print(which)
+    end = time.time()
+    time_elapsed=str(round(end-start,1))
+    return(str(file_one.title[int(which)])+" ("+str(int(100*max_value))+" perc) gefunden in "+time_elapsed+" Sekunden")
 
 
-attribution=[]
+st.title('Querulant')
 
-
-
-query="Logiernaechte"
-
-embeddings1 = model.encode(query, convert_to_tensor=True)
-embeddings2 = model.encode(file_one.text, convert_to_tensor=True)
-print("converted")
-cosine_scores = util.pytorch_cos_sim(embeddings1, embeddings2)
-print(cosine_scores)
-
-
-
-max_value = torch.max(cosine_scores)
-print(max_value)
-which=torch.argmax(cosine_scores)
-print(which)
-
-
-
-attribution.append(str(file_one.title[int(which)])+" ("+str(int(100*max_value))+")")
-
-print(attribution)
+user_input = st.text_input("Suchbegriff eingeben:")
+if(user_input!=""):
+    result=lookup_query(user_input,st.session_state.embeddings2)
+    st.write(result)
