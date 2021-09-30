174b82c26735d106b9db76b8a5a9961080fcc713
Christian Ruiz <ruiz.crp@gmail.com>
2021-09-30 05:19:34 +0000

    timer not working

--- query.py
+++ query.py
@@ -12,7 +12,6 @@ file_one=pd.read_csv("data.csv")
 st.session_state.embeddings2 =  model.encode(file_one.text, convert_to_tensor=True)
 
 def lookup_query(query,embeddings2):
-    start = time.time()
     embeddings1 = model.encode(query, convert_to_tensor=True)
     cosine_scores = util.pytorch_cos_sim(embeddings1, embeddings2)
     #print(cosine_scores)
@@ -21,14 +20,16 @@ def lookup_query(query,embeddings2):
     print(max_value)
     which=torch.argmax(cosine_scores)
     print(which)
-    end = time.time()
-    time_elapsed=str(round(end-start,1))
-    return(str(file_one.title[int(which)])+" ("+str(int(100*max_value))+" perc) gefunden in "+time_elapsed+" Sekunden")
+    return(str(file_one.title[int(which)])+" ("+str(int(100*max_value))+" perc) gefunden in ")
 
 
 st.title('Querulant')
-
 user_input = st.text_input("Suchbegriff eingeben:")
-if(user_input!=""):
+
+if(len(user_input)>0):
+    start = time.time()
     result=lookup_query(user_input,st.session_state.embeddings2)
+    end = time.time()
+    time_elapsed=str(round(end-start,1))
+    result=result+time_elapsed+" Sekunden."
     st.write(result)
